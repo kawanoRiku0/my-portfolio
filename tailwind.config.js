3e3203d33e2bf4067e893cc55964987548f3f1db
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-20 13:32:22 +0900

    setting: tailwindcssにメインカラーを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-purple": "#D08DEB",
+        "main-blue": "#4CC0F5",
+        "main-green": "#6ADE50",
+        "main-yellow": "#F5C34C",
+        "main-red": "#F09C9C",
+      },
+    },
   },
   plugins: [],
 };
