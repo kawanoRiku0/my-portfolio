f1a095fd3f6cf7736581e28a01911f6b511c325d
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-27 09:29:26 +0900

    feat: 下からのフェードインアニメーションを付与するproviderを作成

---
+++ src/components/providers/animation/FadeInFromBelowProvider.tsx
@@ -0,0 +1,42 @@
+import { motion, useAnimation } from "framer-motion";
+import { FC, ReactNode, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const FadeInFromBelowProvider: FC<Props> = ({ children }) => {
+  const control = useAnimation();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    }
+  });
+
+  const fadeInFromBelowVariant = {
+    hidden: { y: 50, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+
+      transition: {
+        duration: 0.5,
+        delay: 0.8,
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      animate={control}
+      initial="hidden"
+      variants={fadeInFromBelowVariant}
+    >
+      {children}
+    </motion.div>
+  );
+};
