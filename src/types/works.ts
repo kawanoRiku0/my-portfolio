e0fe494c2984ed48f9c19e8635c0b5dc9aaf020f
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-28 08:57:53 +0900

    feat: works型を作成

---
+++ src/types/works.ts
@@ -0,0 +1,15 @@
+export type Works = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  image: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  description: string;
+  url: string;
+};
