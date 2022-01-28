7668e69b695eb35c15ec4600241bf5dc6f46e3b7
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-28 11:49:34 +0900

    feat: blogクリック時、投稿先に遷移するように実装

--- src/components/domain/article/molecules/ArticleCard.tsx
+++ src/components/domain/article/molecules/ArticleCard.tsx
@@ -1,5 +1,5 @@
 import { FC, useCallback } from "react";
-import Link from "next/link";
+import Image from "next/image";
 
 import { Article } from "types/article";
 
@@ -12,31 +12,37 @@ const noImage = "/images/no-image.jpg";
 export const ArticleCard: FC<Props> = ({ article }) => {
   // タイトルを35文字以内にトリム
   const trimTitle = useCallback((title: string) => {
-    // タイトルが35文字以上ならトリムしてリターン,35文字以下なら何もせずにリターン
+    // タイトルが35文字以上ならトリム,35文字以下なら何もせずにリターン
     return title.length > 35 ? title.substring(0, 35) + "..." : title;
   }, []);
+
+  // 日付を正しいフォーマットにトリム
+  const trimDate = useCallback((date: string) => {
+    return date.substring(0, 10);
+  }, []);
+
   return (
     <div className=" bg-white rounded-xl shadow-xl overflow-hidden  md:w-[29%] m-3 w-full hover:cursor-pointer hover:scale-105 transition-all">
-      <Link href={`/article/${article.id}`} passHref>
-        <a>
-          {/* Imageコンポーネントを使用することが望ましい、一時的にimgタグ　 */}
-          <img
-            src={article.eye_catch?.url || noImage}
-            alt="アイコン画像"
-            className="w-full object-cover aspect-video"
-          />
-          <div className="px-5 mt-4 pt-2">
-            <h2 className="font-bold mb-6 text-base">
-              {article.title ? trimTitle(article.title) : "記事タイトル"}
-            </h2>
-            <div className="mb-4">
-              <span className=" bg-gray-300  rounded-full px-4 text-sm py-1">
-                {article.tag || "日常"}
-              </span>
-            </div>
+      <a href={article.url}>
+        <Image
+          src={article.image.url || noImage}
+          alt="投稿"
+          layout="intrinsic"
+          objectFit="cover"
+          width={1600}
+          height={900}
+        />
+        <div className="px-5 mt-4 pt-2">
+          <h2 className="font-bold mb-6 text-base">
+            {trimTitle(article.title)}
+          </h2>
+          <div className="mb-4">
+            <span className=" bg-gray-300  rounded-full px-4 text-sm py-1">
+              {trimDate(article.createdAt)}
+            </span>
           </div>
-        </a>
-      </Link>
+        </div>
+      </a>
     </div>
   );
 };
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 import { motion } from "framer-motion";
+import toast, { Toaster } from "react-hot-toast";
 
 import { client } from "libs/microcms/client";
-
 import { Article } from "types/article";
 import { ArticleCard } from "components/domain/article/molecules/ArticleCard";
 import { DefaultLayout } from "components/layouts/DefaultLayout";
@@ -14,21 +14,27 @@ type Props = {
 };
 
 export default function Blog({ articles, error }: Props) {
+  // データ取得時にエラーが発生していれば、トーストで通知
+  if (error) toast.error("データ取得に失敗しました。\n後ほどお試しください。");
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ ease: "easeOut", duration: 0.5 }}
-    >
-      <DefaultLayout pageTitle="BLOG" pageColor="bg-main-blue">
-        <div className="p-4 flex flex-wrap  max-w-5xl justify-between mx-auto after:content-[''] after:block after:w-[29%] after:m-3">
-          {articles?.map((article) => {
-            return <ArticleCard key={article.id} article={article} />;
-          })}
-        </div>
-      </DefaultLayout>
-    </motion.div>
+    <>
+      {/* データ取得失敗時、トーストで通知 */}
+      <Toaster />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: "easeOut", duration: 0.5 }}
+      >
+        <DefaultLayout pageTitle="BLOG" pageColor="bg-main-blue">
+          <div className="p-4 flex flex-wrap  max-w-5xl justify-between mx-auto after:content-[''] after:block after:w-[29%] after:m-3">
+            {articles?.map((article) => {
+              return <ArticleCard key={article.id} article={article} />;
+            })}
+          </div>
+        </DefaultLayout>
+      </motion.div>
+    </>
   );
 }
 
@@ -42,12 +48,11 @@ export const getServerSideProps = async () => {
       },
     };
   } catch (e) {
-    // データ取得失敗時エラーをpropsに渡す。
-    // 現在上手く渡せていないので修正が必要
-    const error = JSON.stringify(e);
+    // エラー発生時、ターミナルにエラーを表示
+    console.log(e);
     return {
       props: {
-        error,
+        error: true,
       },
     };
   }
